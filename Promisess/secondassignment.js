75accbe4210ed9b4a6a5600785a224a125ad4f12
Uddeshy Manoj Jaiswal <uddeshkrrish@gmail.com>
2025-07-29 11:22:02 +0530

    completedassignment

---
+++ Promisess/secondassignment.js
@@ -0,0 +1,27 @@
+
+function fetchData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const isSuccess = Math.random() > 0.5; 
+      if (isSuccess) {
+        resolve("Data fetched successfully!");
+      } else {
+        reject("Failed to fetch data");
+      }
+    }, 1000); 
+  });
+}
+
+
+async function fetchDataHandler() {
+  try {
+    const result = await fetchData();
+    console.log("Fetched data successfully!");
+    console.log(result);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+
+fetchDataHandler();
