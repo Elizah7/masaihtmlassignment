a64e4281162021cabda0a1a0da76519f4bb117ea
Uddeshy Manoj Jaiswal <uddeshkrrish@gmail.com>
2025-07-29 13:06:02 +0530

    completed

---
+++ Promisess/thirdassignment.js
@@ -0,0 +1,20 @@
+function checkEvenNumber(num) {
+  return new Promise((resolve, reject) => {
+    if (typeof num !== "number" || isNaN(num)) {
+      reject(`${num} is odd or invalid`);
+    } else if (num % 2 === 0) {
+      resolve(`${num} is even`);
+    } else {
+      reject(`${num} is odd or invalid`);
+    }
+  });
+}
+
+
+checkEvenNumber(4)
+  .then(console.log)    
+  .catch(console.error);
+
+checkEvenNumber(5)
+  .then(console.log)    
+  .catch(console.error);
